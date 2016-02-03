12e548cccf814339a077c444703308f0dae10022
emilkayumov <emil.kayumov@gmail.com>
2016-02-03 13:29:56 +0500

    adding first version of knn

--- data_recovery.py
+++ data_recovery.py
@@ -1,11 +1,9 @@
 import numpy as np
+from scipy.stats.mstats import mode
 
 
 def svd_imputer(data, rank=None, max_iter=30, tol=1e-1):
-
     # https://web.stanford.edu/~hastie/Papers/missing.pdf
-
-    from scipy.stats.mstats import mode
 
     X = np.array(data)
     mask = X != X
@@ -13,7 +11,6 @@ def svd_imputer(data, rank=None, max_iter=30, tol=1e-1):
     # first inputing by most common
     for i in range(X.shape[1]):
         X[mask[:, i], i] = mode(X[np.logical_not(mask[:, i]), i])[0][0]
-
 
     # iteratively using svd for best approximation
     for i in range(max_iter):
@@ -30,7 +27,43 @@ def svd_imputer(data, rank=None, max_iter=30, tol=1e-1):
             break
         X[mask] = new_X[mask]
 
-
     X[mask] = new_X[mask]
+
+    return X
+
+
+def knn_imputer(data, n_neighbors=1, metric='l2'):
+    X = np.array(data)
+    mask = X != X
+    objects = mask.sum(axis=1) != 0
+
+    # without missing values
+    X_full = X[np.logical_not(objects)]
+
+    for i, obj in enumerate(objects):
+        if not obj:
+            continue
+
+        mask_obj = np.logical_not(mask[i])
+
+        # finding nearest
+        if metric == 'l2':
+            neighbors = ((X_full[:, mask_obj] - X[i, mask_obj]) ** 2).sum(axis=1).argsort()[:n_neighbors]
+        elif metric == 'l1':
+            neighbors = ((X_full[:, mask_obj] - X[i, mask_obj]).abs()).sum(axis=1).argsort()[:n_neighbors]
+        else:
+            distance = np.zeros(X_full.shape[0])
+            for j in range(X_full.shape[0]):
+                distance[j] = metric(X[i, mask_obj], X_full[j, mask_obj])
+            neighbors = distance.argsort()[:n_neighbors]
+
+        X_neighbors = X_full[neighbors]
+
+        # replacing missing values by most common
+        for j, feat in enumerate(mask_obj):
+            if feat:
+                continue
+
+            X[i, j] = mode(X_neighbors[:, j])[0][0]
 
     return X
